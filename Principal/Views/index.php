55db4ecfce0a53b31ddc62991e4094219b99a3be
Adri <keyton962@gmail.com>
2024-12-20 14:05:23 +0100

    cambios estilos menores

--- Principal/Views/index.php
+++ Principal/Views/index.php
@@ -8,7 +8,7 @@
         <meta name="description" content="" />
         <meta name="author" content="" />
         <title>SP500 GAMES</title>
-        <link rel="icon" type="image/x-icon" href="./android-chrome-512x512.png" />
+        <link rel="icon" type="image/x-icon" href="../../assets/favicon.png" />
         <!-- Font Awesome icons (free version)-->
         <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
         <!-- Google fonts-->
