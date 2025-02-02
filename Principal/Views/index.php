c73b14911e6b59fdd0f2f768b7197298b89eb18d
Bryan Cuadrado <cuadradobryan@gmail.com>
2025-02-02 18:22:44 +0100

    sads

--- Principal/Views/index.php
+++ Principal/Views/index.php
@@ -1,5 +1,62 @@
 <!DOCTYPE html>
 <html lang="en">
+<<<<<<< Updated upstream
+=======
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>SP500 GAMES</title>
+        <link rel="icon" type="image/x-icon" href="../../assets/favicon.png" />
+        <!-- Font Awesome icons (free version)-->
+        <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
+        <!-- Google fonts-->
+        <link href="https://fonts.googleapis.com/css?family=Varela+Round" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link rel="stylesheet" href="../../css/styles.css">
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+            <div class="container px-4 px-lg-5">
+                <a class="navbar-brand" href="#page-top">SP 500 GAMES</a>
+                <button class="navbar-toggler navbar-toggler-right" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                    Menu
+                    <i class="fas fa-bars"></i>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+  <ul class="navbar-nav ms-auto">
+    <li class="nav-item">
+        <!-- Aqui cada input enviara una señal al controlador-->
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+  <form action="../Controller/controlador.php" method="post" class="d-flex w-100">
+    <ul class="navbar-nav ms-auto">
+      <li class="nav-item">
+        <button type="submit" name="tmp_inicio_btn_entrar_home" value="index" class="nav-link btn btn-link">Inicio</button>
+      </li>
+      <li class="nav-item">
+        <button type="submit" name="tmp_inicio_btn_entrar_perfil" value="perfil" class="nav-link btn btn-link">Perfil</button>
+      </li>
+      <li class="nav-item">
+        <button type="submit" name="tmp_inicio_btn_entrar_login" value="login" class="nav-link btn btn-link">Login</button>
+      </li>
+      <li class="nav-item">
+        <button type="submit" name="tmp_inicio_btn_entrar_registro" value="registro" class="nav-link btn btn-link">Registro</button>
+      </li>
+      <!-- Más botones se pueden agregar aquí -->
+      <!-- Ejemplo:
+      <li class="nav-item">
+        <button type="submit" name="page" value="admin" class="nav-link btn btn-link">Admin</button>
+      </li>
+      -->
+      <?php if (isset($_SESSION['usuarioLogueado']['rol']) && $_SESSION['usuarioLogueado']['rol'] === 'Lider'): ?>
+    <li class="nav-item">
+        <button type="submit" name="tmp_inicio_btn_entrar_Administrador" value="Administrador" class="nav-link btn btn-link">Administrador</button>
+    </li>     
+    <?php endif; ?>
+>>>>>>> Stashed changes
 
 <head>
     <meta charset="utf-8" />
